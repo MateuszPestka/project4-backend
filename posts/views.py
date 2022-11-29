2c17b97c6ea9302a9cb08d8cab2c17594df0d90e
MateuszPestka <pes.matthew@gmail.com>
2022-11-29 04:26:30 +0000

    Add views to posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(APIView):
+    def get(slef, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, contect={'request': request}
+        )
+        return Response(serializer.data)
