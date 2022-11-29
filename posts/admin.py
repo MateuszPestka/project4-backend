188ff62c0af6527ddbeb22f7f91cb562008562cd
MateuszPestka <pes.matthew@gmail.com>
2022-11-29 09:34:03 +0000

    Add frameworks for deployment

--- posts/admin.py
+++ posts/admin.py
@@ -2,6 +2,7 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import Post
 from django.contrib import admin
 
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
