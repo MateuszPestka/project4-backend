1cab43064e516228f43aa8fa9abf4312e28c78e1
MateuszPestka <pes.matthew@gmail.com>
2022-11-29 09:37:48 +0000

    Deployment Commit

--- written_api/settings.py
+++ written_api/settings.py
@@ -50,12 +50,12 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'django_summernote',
-    'rest_framework.authtoken', 
+    'rest_framework.authtoken',
     'dj_rest_auth',
-    'django.contrib.sites', 
-    'allauth', 
-    'allauth.account', 
-    'allauth.socialaccount', 
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'dj_rest_auth.registration',
 
 
@@ -79,6 +79,7 @@ JWT_AUTH_COOKIE = 'my-app-auth'
 
 JWT_AUTH_SECURE = True
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'written_api.serializers.CurrentUserSerializer'}
 
 
 MIDDLEWARE = [
